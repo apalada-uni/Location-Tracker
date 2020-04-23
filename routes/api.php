a960a1642149e86007043b08654c20d8a51d79d4
Albert <apalada@arris.com.au>
2020-04-23 17:00:14 +0930

    Added API Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/userlocation', 'UserLocationController');
+
+Route::resource('/location', 'LocationController');
+
+Route::get('/lastcheckin', 'UserLocationController@lastCheckIn');
